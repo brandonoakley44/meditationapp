67700b3678f90f3530469574439af3bdf8ea8818
brandonoakley44 <brandonoakley324@gmail.com>
2024-09-28 10:38:45 -0300

    affirmations screen ui

--- app/(tabs)/affirmations.tsx
+++ app/(tabs)/affirmations.tsx
@@ -1,10 +1,26 @@
+import AppGradient from '@/components/AppGradient';
+import GuidedAffirmationsGallery from '@/components/GuidedAffirmationsGallery';
+import AFFIRMATION_GALLERY from '@/constants/affirmation-gallery';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, View, Text } from 'react-native';
 
 const Affirmations = () => {
     return (
-        <View>
-            
+        <View className='flex-1'>
+            <AppGradient
+                colors={["#2e1f58", "#54426b", "#a790af" ]}
+            >
+                <ScrollView
+                showsVerticalScrollIndicator={false}
+                >
+                    <Text className="text-zinc-50 text-3xl font-bold">Change your beliefs with affirmations</Text>
+                    <View>
+                        {AFFIRMATION_GALLERY.map((g) => (
+                            <GuidedAffirmationsGallery key={g.title} title={g.title} previews={g.data} />
+                        ))}
+                    </View>
+                </ScrollView>
+            </AppGradient>
         </View>
     );
 }
